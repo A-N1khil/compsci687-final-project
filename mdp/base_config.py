fbac4f0a821d66d928f42c6f2febc1cf54275c47
A-N1khil <nikhilanand1006@gmail.com>
2025-11-29 19:16:07 -0500

    Added domain

--- mdp/base_config.py
+++ mdp/base_config.py
@@ -2,10 +2,7 @@ import random
 
 class BaseConfig:
     def __init__(self, seed=None):
-        if seed is not None:
-            self.rng = random.Random(seed)
-        else:
-            self.rng = random.Random()
+        self.rng: random.Random = random.Random(seed)
 
-    def get_rng(self):
+    def get_rng(self) -> random.Random:
         return self.rng
